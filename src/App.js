10c1eee0772dea837722c9be3aaea3c3177eecd9
Mikhail Afonin <afoninmv@gmail.com>
2021-06-11 19:59:13 +0300

    feat(part_19-20): fetch data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w'
-        },
-      ]
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
+  }
+
   render() {
   return (
     <div className="App">
